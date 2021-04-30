6806643c2a6f7942465640a653907a5485d1e000
Joseph Monreal <joemonreal23@icloud.com>
2021-04-29 18:41:32 -0700

    Configuring Webpack for portolfio project. Currently compiles code and is working as expected.

---
+++ src/client/components/App.js
@@ -0,0 +1,21 @@
+// React, Axios, React-Route
+import React from 'react';
+import ReactDOM from 'react-dom';
+class App extends React.Component {
+  // constuctor(props) {
+  //   super(props);
+  //   this.state = {
+
+  //   };
+  // }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports ={
+  mode: "development",
+  entry: {
+    main: "./src/index.js"
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ]
+}
