a77fbc015926af6d870e4ac6f7e852d0c93694a4
Joseph Monreal <joemonreal23@icloud.com>
2021-05-03 09:44:20 -0700

    Setup webpack to use css-loader and style-loader. This will allow me to style my portfolio and bundle however many style sheets.

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,18 +1,18 @@
 // React, Axios, React-Route
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Nav from './Nav';
+import Footer from './Footer';
 class App extends React.Component {
-  // constuctor(props) {
-  //   super(props);
-  //   this.state = {
-
-  //   };
-  // }
-
   render() {
+    const style = {
+      color: 'blue'
+    }
     return (
       <div>
-        <h1>Portfolio</h1>
+        <Nav />
+        <h1 style={style}>Portfolio</h1>
+        <Footer />
       </div>
     )
   }
---
+++ src/client/components/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <h1>Footer</h1>
+    )
+  }
+}
+export default Footer;
