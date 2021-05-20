2f1c06b347258b440de697942bd3e0d8844aa213
Joseph Monreal <joemonreal23@icloud.com>
2021-05-20 15:03:31 -0700

    Footer component complete.

--- src/client/components/footer.js
+++ src/client/components/footer.js
@@ -10,17 +10,23 @@ const Footer = () => {
     { social: "Email", href: "" },
   ]);
   return (
-    <Container fluid>
+    <Container fluid className="p-5 m-5">
       <Row>
-        <Col xs={2} sm={2} md={2} lg={2} xl={2}>
-          <p className="h5">Designed & Developed by Joseph Monreal 2021</p>
+        <Col xs={12} sm={12} md={6} lg={6} xl={6}>
+          <p style={{ fontSize: "10px" }} className="pt-2">
+            Designed & Developed by Joseph Monreal 2021
+          </p>
         </Col>
-        <Col>
-          {links.map((ele, idx) => (
-            <>
-              <a href={ele.href}>{ele.social}</a>{" "}
-            </>
-          ))}
+        <Col md={6} lg={6} xl={6}>
+          <Row>
+            {links.map((ele, idx) => (
+              <Col className="p-0 m-0">
+                <a style={{ fontSize: "10px" }} href={ele.href}>
+                  {ele.social}
+                </a>{" "}
+              </Col>
+            ))}
+          </Row>
         </Col>
       </Row>
     </Container>
